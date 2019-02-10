b69954f4817bbfb718597e3b1d42cbf26f8637b1
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-02-10 13:16:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** https://goo.gl/ZBfUKv    pixel حياكم سيرفر **[ " ${x} " ]`)
+        message.channel.send(`** https://discord.gg/NSXJzjp    pixel حياكم سيرفر **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`** https://goo.gl/ZBfUKv    pixel حياكم سيرفر **[ " ${x} " ]`)
+        message.channel.send(`** https://discord.gg/NSXJzjp    pixel حياكم سيرفر **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
